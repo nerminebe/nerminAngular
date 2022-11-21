d7b56e84d89c0e0b52ed5592fbd87fe99df772ba
nerminbenmohamed <nermin.benmohamed@esprit.tn>
2022-11-21 20:59:04 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponentComponent } from './products-component/products-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponentComponent
   ],
   imports: [
     BrowserModule,
